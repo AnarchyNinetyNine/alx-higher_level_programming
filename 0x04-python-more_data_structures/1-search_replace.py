afa602c2dfe022e3c87134ca59f6e054ee5438d9
Idris <franckenstein999@gmail.com>
2024-03-20 19:48:46 -0700

    adding 1-search_replace.py enhanced version using map

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if not my_list or search not in my_list:
-        return None
-    new_list = list(map(lambda elem : elem if elem != search else replace, my_list))
+    if not my_list or search not in my_list or not replace:
+        return my_list
+    new_list = list(
+            map(lambda elem : elem if elem != search else replace, my_list))
     return new_list
