f46c4a23cc0dd659e9c19ed85c5f30f6231dd6dd
Idris <franckenstein999@gmail.com>
2024-04-16 16:31:10 -0700

    Updating print format

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -38,6 +38,5 @@ def read_file(filename=""):
         whitespace characters (like '\n').
 
     """
-    with open(filename, encoding='utf-8') as text_file:
-        for line in text_file:
-            print(line.strip())
+    with open(filename, 'rt') as text_file:
+            print(text_file.read(), end="")
