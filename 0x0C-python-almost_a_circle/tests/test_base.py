e062ecc183cf4dbecb51eee1712adf95ceca5f34
Idris <franckenstein999@gmail.com>
2024-04-22 17:08:48 -0700

    add some tests

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,16 @@
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    def test_id_assignment(self):
+        obj1 = Base()
+        obj2 = Base()
+        self.assertEqual(obj1.id, 1)
+        self.assertEqual(obj2.id, 2)
+
+    def test_custom_id(self):
+        obj = Base(id=100)
+        self.assertEqual(obj.id, 100)
+
+if __name__ == '__main__':
+    unittest.main()
