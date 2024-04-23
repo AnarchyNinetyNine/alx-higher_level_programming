22152e15174487fc7ca68d52e930ccafc478d48a
Idris <franckenstein999@gmail.com>
2024-04-22 18:30:20 -0700

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -36,10 +36,31 @@ class Rectangle(Base):
                                position (default is 0).
             id (int, optional): The ID of the rectangle (default is None).
         """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be > 0")
         self.width = width
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be > 0")
+
         self.height = height
+
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be > 0")
         self.x = x
+
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be > 0")
         self.y = y
+
         super().__init__(id)
 
         @property
