23e96147d4196f3e5663dcec61be545c3c3a4538
Idris <franckenstein999@gmail.com>
2024-04-24 13:48:25 -0700

    Add Validation to all setter methods and instantiation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -38,6 +38,19 @@ class Rectangle(Base):
         """
         super().__init__(id)
 
+        inpStr = ["width", "height", "x", "y"]
+        inp = [width, height, x, y]
+        sign = ""
+
+        for inp_i, inpStr_i in zip(inp, inpStr):
+
+            if not isinstance(inp_i, int):
+                raise TypeError(f"{inpStr_i} must be an integer")
+
+            if inp_i < 0:
+                sign = ">=" if inpStr_i == "x" or inpStr_i == "y" else ">"
+                raise ValueError(f"{inpStr_i} must be {sign} 0")
+
         self.width = width
         self.height = height
         self.x = x
@@ -66,19 +79,35 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Setter method for width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @height.setter
     def height(self, value):
         """Setter method for height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @x.setter
     def x(self, value):
         """Setter method for x."""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @y.setter
     def y(self, value):
         """Setter method for y."""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
