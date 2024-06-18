ebbe7e11de1bf5c1fcfd618e8a28fab91fd47f8b
Idris <franckenstein999@gmail.com>
2024-06-18 15:54:21 -0700

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const process = require('process');
+const args = process.argv;
+const x = parseInt(args[2], 10);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
+
