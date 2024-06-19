30a31c1dc37e5df1b2da9ddeb952e1099b1b734e
Idris <franckenstein999@gmail.com>
2024-06-18 17:50:11 -0700

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,6 +6,10 @@ const args = process.argv.slice(2);
 if (args.length <= 1) {
   console.log(0);
 } else {
-  args.sort();
+  for (let i = 0; i < args.length; i++) {
+    args[i] = parseInt(args[i], 10);
+  }
+
+  args.sort((a, b) => b - a);
   console.log(args[1]);
 }
