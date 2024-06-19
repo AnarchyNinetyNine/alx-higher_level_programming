f763e2fecac0d05c0517df39712caa7435282f88
Idris <franckenstein999@gmail.com>
2024-06-18 17:31:55 -0700

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const process = require('process');
+const num = parseInt(process.argv[2], 10);
+
+function factorial (num) {
+  if (isNaN(num) || num === 0) {
+    return 1;
+  }
+
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(num));
