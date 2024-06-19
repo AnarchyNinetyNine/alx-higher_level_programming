5d23a093071d8ca05274e4a7577d03a07231c886
Idris <franckenstein999@gmail.com>
2024-06-18 17:01:30 -0700

    add function add(a, b)

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,4 +4,8 @@ const process = require('process');
 const num1 = parseInt(process.argv[2], 10);
 const num2 = parseInt(process.argv[3], 10);
 
-console.log(num1 + num2);
+function add (a, b) {
+  console.log(a + b);
+}
+
+add(num1, num2);
