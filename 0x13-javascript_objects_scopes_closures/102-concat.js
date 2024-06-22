a63229da004cf6e7ba42ca737e93bf78a9f30b8f
Idris <franckenstein999@gmail.com>
2024-06-21 18:42:28 -0700

    task 13

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const destination = process.argv[4];
+
+fs.readFile(fileA, 'utf8', (err, dataA) => {
+  if (err) throw err;
+
+  fs.readFile(fileB, 'utf8', (err, dataB) => {
+    if (err) throw err;
+
+    fs.writeFile(destination, dataA + dataB, 'utf8', (err) => {
+      if (err) throw err;
+    });
+  });
+});
