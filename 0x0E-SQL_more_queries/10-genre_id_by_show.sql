66aa0e4251aea281127f41b38a04f425206e4a41
Idris <franckenstein999@gmail.com>
2024-06-23 15:11:10 -0700

    Task 10: Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,10 +2,8 @@
 -- A script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
-FROM tv_shows, tv_genre
-WHERE tv_show_genres.genre_id IN (
-    SELECT genre_id
-    FROM tv_show_genres
-    WHERE COUNT(genre_id) >= 1
-)
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.genre_id
+HAVING COUNT(tv_show_genres.genre_id) >= 1
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
