636b12fc503231da8bf24414cf79a4679a4738d5
Idris <franckenstein999@gmail.com>
2024-06-23 13:36:08 -0700

    Task 3: Always a name

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- Script: 3-force_name.sql
+-- Script to create table force_name in MySQL database hbtn_0d_2
+
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
