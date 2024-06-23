9d54945c8c3446912f4e3362bdbad9925ee7b6b9
Idris <franckenstein999@gmail.com>
2024-06-23 14:38:05 -0700

    Task 9: Cities by States

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Script: 9-cities_by_state_join.sql
+-- A script that lists all cities contained in the database hbtn_0d_usa.
+
+SELECT cities.id AS id, cities.name AS name, states.name AS name
+FROM cities
+JOIN states
+ON cities.id = states.id;
