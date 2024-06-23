86e729e7ceae861244e914d8037715d9aa7210b6
Idris <franckenstein999@gmail.com>
2024-06-22 18:52:45 -0700

    Task 8: Count 89

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Script: 8-count_89.sql
+-- Script to display the number of records with id = 89 in the table first_table in the database hbtn_0c_0
+
+SELECT COUNT(id) FROM first_table WHERE id = 89;
