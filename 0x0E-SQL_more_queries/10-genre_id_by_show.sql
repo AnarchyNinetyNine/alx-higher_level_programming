392d6303daba13502e5258a36bc16717ed8f42c5
Idris <franckenstein999@gmail.com>
2024-06-23 15:14:39 -0700

    Task 10: Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,5 +5,4 @@ SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows
 JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.genre_id
-HAVING COUNT(tv_show_genres.genre_id) >= 1
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
