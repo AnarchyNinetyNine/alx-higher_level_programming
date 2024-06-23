9e4d1c18249e68a8dfe06e19a653326aacf074ac
Idris <franckenstein999@gmail.com>
2024-06-23 08:30:42 -0700

    Task 15: Number by score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+-- Script: 15-groups.sql
+-- Script to list the number of records with the same score in the table second_table
+
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
