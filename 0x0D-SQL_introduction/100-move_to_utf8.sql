02b1973cd6a4ee5abe03696786ca670031e3f420
Idris <franckenstein999@gmail.com>
2024-06-23 08:42:33 -0700

    Task 17: Go to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,11 @@
+-- Script: 100-move_to_utf8.sql
+-- Script to convert hbtn_0c_0 database, first_table, and name field in first_table to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+
+-- Convert the database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+-- Convert the table to UTF8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the name field in first_table to UTF8
+ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
