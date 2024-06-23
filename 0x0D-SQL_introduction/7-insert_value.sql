409a040d3348498465a98d24252d5596962d28f5
Idris <franckenstein999@gmail.com>
2024-06-22 18:50:15 -0700

    Task 7: First add

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
--- Script: insert_row.sql
+-- Script: 7-insert_value.sql
 -- Script to insert a new row into the table first_table in the database hbtn_0c_0
 
 INSERT INTO first_table VALUES (89, "Best School");
