9f393f2d0858e264dd2993ca9bc0f5be2e64eeb5
Idris <franckenstein999@gmail.com>
2024-06-23 13:50:22 -0700

    Task 6: States table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,14 @@
+- Script: 6-states.sql
+-- Script to create database hbtn_0d_usa and table states in MySQL server
+
+-- Create database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Switch to the database hbtn_0d_usa
+USE hbtn_0d_usa;
+
+-- Create table states if it does not exist
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
