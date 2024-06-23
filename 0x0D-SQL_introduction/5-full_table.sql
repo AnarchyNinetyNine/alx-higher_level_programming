6b4edce64edd49b57bc6b184c1cdb8088adf5348
Idris <franckenstein999@gmail.com>
2024-06-22 18:20:27 -0700

    Task 5: Full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,7 +1,4 @@
 -- Script: 5-full_table.sql
 -- This script prints the full description of the table first_table from the specified database.
 
-SELECT TABLE_NAME AS 'Table', CREATE_TABLE
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-AND TABLE_NAME = 'first_table';
+DESC first_table;
